372bd67f7bb585de327ebe4d2c0a4e39daab2d3b
Devin Olenick <hoyatown@yahoo.com>
2021-10-28 21:15:18 -0400

    app updates

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import Main from './components/MainComponent';
-import Weather from './components/weather';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import './App.css';
-export default function App() {
 
 const store = ConfigureStore();
 
-const [lat, setLat ] = useState([]);
-const [long, setLong] = useState([]);
-const [data, setData] = useState([]);
-
-useEffect(() => {
-  const fetchData = async () => {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-
-    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-    .then(res => res.json())
-    .then(result => {
-      setData(result)
-      console.log(result);
-    });
-  }
-  fetchData();
-},
-
-  [lat, long])
 
 class App extends Component {
   render() {
@@ -39,14 +15,12 @@ class App extends Component {
         <BrowserRouter>
           <div className="App">
             <Main />
-            {(typeof data.main != 'undefined') ? (
-        <Weather weatherData={data}/>
-      ): (
-        <div></div>
-      )}
+           
           </div>
         </BrowserRouter>
       </Provider>
     );
-  }};
+  };
 }
+
+export default App;
